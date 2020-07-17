15024a0b8e4e5bc506d3f50ffd0196c8c65ed89d
Juraj Jakubik <juraj.jakubik@tieto.com>
2020-07-17 14:29:01 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,55 @@
+(function ($) {
+  "use strict";
+  // TOP Menu Sticky
+  $(window).on('scroll', function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 400) {
+      $("#sticky-header").removeClass("sticky");
+      $('#back-top').fadeIn(500);
+    } else {
+      $("#sticky-header").addClass("sticky");
+      $('#back-top').fadeIn(500);
+    }
+  });
+
+
+  $(document).ready(function () {
+
+    // mobile_menu
+    var menu = $('ul#navigation');
+    if (menu.length) {
+      menu.slicknav({
+        prependTo: ".mobile_menu",
+        closedSymbol: '+',
+        openedSymbol: '-',
+        closeOnClick: true
+      });
+    };
+
+    /* magnificPopup img view */
+    $('.popup-image').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+
+    /* magnificPopup video view */
+    $('.popup-video').magnificPopup({
+      type: 'iframe'
+    });
+
+
+    if (document.getElementById('default-select')) {
+      $('select').niceSelect();
+    }
+
+
+  });
+  //------- Mailchimp js --------//
+  function mailChimp() {
+    $('#mc_embed_signup').find('form').ajaxChimp();
+  }
+  mailChimp();
+
+})(jQuery);

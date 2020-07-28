7fd2414c6353416efddddbd9aad0a84379dbe869
Juraj Jakubik <juraj.jakubik@tieto.com>
2020-07-28 15:18:31 +0200

    Almost final

--- js/main.js
+++ js/main.js
@@ -15,6 +15,16 @@
 
   $(document).ready(function () {
 
+    if ($(window).width() > 767) {
+      $('#map-iframe').attr('src', 'https://www.google.com/maps/embed/v1/place?q=place_id:ChIJje_T-1CUEkcRbWiPdOp-fhM&key=AIzaSyDqvcZbtfvXpL6eqUV95z1v3diMkSZD6dw');
+    }
+
+    $('#clock').countdown('2020/09/12 15:00:00', function (event) {
+      $(this).html(event.strftime(
+        '<div class="countdown_wrap d-flex"><div  class="single_countdown"><h3>%D</h3><span>Dny</span></div><div class="single_countdown"><h3>%H</h3><span>Hodiny</span></div><div class="single_countdown"><h3>%M</h3><span>Minuty</span></div><div class="single_countdown"><h3>%S</h3><span>Sekundy</span></div></div>'
+      ));
+    });
+
     // mobile_menu
     var menu = $('ul#navigation');
     if (menu.length) {
